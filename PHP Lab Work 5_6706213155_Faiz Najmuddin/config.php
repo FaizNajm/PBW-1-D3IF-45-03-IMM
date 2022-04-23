0cf1f2e4c973c98f278ad36735999b1c223ec567
FaizNajm <faiznajmuddin@student.telkomuniversity.ac.id>
2022-04-23 14:49:01 +0700

    push Lab Work 5

---
+++ PHP Lab Work 5_6706213155_Faiz Najmuddin/config.php
@@ -0,0 +1,14 @@
+<?php
+$server = "localhost";
+$username = "root";
+$pass = "";
+$dbname = "modul5";
+
+$conn = mysqli_connect($server, $username, $pass, $dbname);
+
+if (!$conn) {
+    die ("Conncetion failed: " . mysqli_connect_error());
+} else {
+    //echo "Connected Successfully ";
+}
+?>
